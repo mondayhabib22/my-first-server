1c96491f0d149473f477b1c980dd0b101dbf8040
Monday Habib <mondayhabib3@gmail.com>
2026-03-02 14:52:20 +0100

    Sever

--- my-first-server/server.js
+++ my-first-server/server.js
@@ -1,8 +1,13 @@
 // Creating first server
-const http = require("htttp");
+const http = require("http");
 
 // create a server
 const server = http.createServer((req, res) => {
   res.write("hello world, i am in new horizon from Node server!");
-  res.end()
+  res.end();
+});
+
+// Starting the server
+server.listen(3000, () => {
+  console.log("Server running on port 3000");
 });
